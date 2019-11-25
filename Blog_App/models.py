bb093cdc17636af7a12af3d6f9e7775a9bf34255
ketan <ketanpathak@outlook.com>
2019-11-25 21:55:08 +0530

    first commit

---
+++ Blog_App/models.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from django.db import models
+from django.utils import timezone
+from django.db.models.signals import pre_save
+from django.utils.text import slugify
+
+
+class CustomManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status='published')
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (('draft', 'Draft'), ('published', 'Published'))
+    title = models.CharField(max_length=260)
+    slug = models.SlugField(max_length=264, unique_for_date='publish')
+    author = models.ForeignKey(User, related_name='blog_post', on_delete=models.PROTECT)
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='draft')
+    objects = CustomManager()
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='Comment', on_delete=models.PROTECT)
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+    comment = models.TextField()
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    slug = slugify(instance.title)
+    exists = Post.objects.filter(slug=slug).exists()
+    if exists:
+        slug = "%s-%s" % (slug, instance.id)
+    instance.slug = slug
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
